720fa7286ecf001856e251dedd34c716594326b7
James Steves <jameskam40@gmail.com>
2024-04-18 15:28:21 -0300

    Update index.js
    Today we finish the project.

--- feedback-ui/feedback-UI/feedback-ui/index.js
+++ feedback-ui/feedback-UI/feedback-ui/index.js
@@ -1,9 +1,44 @@
-const ratingEls = document.querySelectorAll(".rating");
+const ratingEls = document.querySelectorAll(".rating")
+
+const btnEl = document.getElementById("btn")
+
+const containerEl = document.getElementById("container")
+
+
+
+let selectedRating = "";
 
 
 ratingEls.forEach((ratingEl) => {
     ratingEl.addEventListener("click", (event) => {
-        console.log(event.target. innerText || event.
-        target.parentNode.innerText);
-    });
+       removeActive();
+        selectedRating = event.target.innertext || event.target.
+        parentNode.innertext;
+        event.target.classList.add("active");
+        event.target.parentNode.classList.add("active");
+    })
 });
+
+btnEl.addEventListener("click", ()=>{
+    if (selectedRating !== ""){
+        containerEl.innerHTML = `
+         <strong> Thank you ! </strong>
+         <br>
+         <br>
+         <strong>
+         Feedback : ${selectedRating}
+         </strong>
+         <p>
+         We'll use your feedback to improve our custumer support.
+         </p>
+        `;
+    }
+})
+
+
+function removeActive(){
+ ratingEls.forEach((ratingEl)=>{
+    ratingEl.classList.remove("active");
+})
+
+}
